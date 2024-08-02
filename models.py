f98de3df84880fc217eb72404a46dd23577d454c
赵森森 <451682479@qq.com>
2024-08-02 17:33:30 +0800

    这是什么寄吧玩意

---
+++ models.py
@@ -0,0 +1,36 @@
+import torch.cuda
+from torch import nn
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+
+)
+
+print(device)
+
+# 这是什么寄吧玩意
+class NeuralNetwork(nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork().to(device)
+print(model)
