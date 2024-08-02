a409f47162d7be5a4305c498be7d125982e3bf29
赵森森 <451682479@qq.com>
2024-08-02 17:58:43 +0800

    class 2

--- class-practice/class-practice.py
+++ class-practice/class-practice.py
@@ -1,15 +1,15 @@
 class MyClass:
-    def __init__(self, x, y):
+    def __init__(self, x):
         super().__init__()
         self.x = x
-        self.y = y
 
-    def add(self):
-        return self.x + self.y
+    def add(self, item):
+        return self.x + item.x
 
 
-myclass = MyClass(1, 2)
+myclass = MyClass(1)
+myclass2 = MyClass(20)
 
-c = myclass.add()
+c = myclass.add(myclass2)
 
 print(c)
