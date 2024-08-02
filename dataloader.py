adf2f0a9ad4ee5266ff0c6732dd7fc6b6fcba51e
赵森森 <451682479@qq.com>
2024-08-02 17:26:56 +0800

    dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,1 +1,20 @@
-# dataLoaders
+# 使用dataSet
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+
+train_data = datasets.FashionMNIST(
+    "./",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+train_dataloader = DataLoader(train_data, batch_size=64, shuffle=True)
+
+# 遍历 数据
+for x, y in train_dataloader:
+    # Shape of X[N, C, H, W]: torch.Size([64, 1, 28, 28])
+    print(x.shape)
+    # Shape of y: torch.Size([64]) torch.int64
+    print(y.dtype)
